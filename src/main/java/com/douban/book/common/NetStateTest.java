a4d64db0b6a587d85657499cfdf50959389aae48
duqingqing <dulovefighting@sina.com>
2018-12-21 11:03:31 +0800

    添加网络状态检测和配置文件

---
+++ src/main/java/com/douban/book/common/NetStateTest.java
@@ -0,0 +1,9 @@
+package com.douban.book.common;
+
+/**
+ * @Auther: duqingqing
+ * @Date: 18-12-21 11:01
+ * @Description:
+ */
+public class NetStateTest {
+}
---
+++ src/main/java/com/douban/book/controller/BookInformationController.java
@@ -0,0 +1,9 @@
+package com.douban.book.controller;
+
+/**
+ * @Auther: duqingqing
+ * @Date: 18-12-21 10:24
+ * @Description:
+ */
+public class BookInformationController {
+}
